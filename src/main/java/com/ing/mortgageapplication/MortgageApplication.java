683eaffce24656c0097a343307e8070b31feaa94
jaiswal <neelesh.jaiswal@webfleet.com>
2025-01-24 21:17:07 +0100

    Initial commit

--- src/main/java/com/ing/mortgageapplication/MortgageApplication.java
+++ src/main/java/com/ing/mortgageapplication/MortgageApplication.java
@@ -3,7 +3,7 @@ package com.ing.mortgageapplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ing.mortgageapplication")
 public class MortgageApplication {
 
     public static void main(String[] args) {
